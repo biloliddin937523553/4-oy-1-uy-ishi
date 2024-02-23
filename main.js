2c18a51d60800052afd67cc1bf6c4f8ff62573ef
biloliddin937523553 <biloliddinlatipov0@gmail.com>
2024-02-23 21:37:24 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let name = prompt('Ismingizni kiriting')
+let surname = prompt('Familiyangizni kiriting')
+let age = prompt('Yoshingizni kiriting')
+let fan = prompt('Yoqtirgan faningizni kiriting')
+let adres = prompt('Yashaydigan tumaningizni kiriting')
+
+
+console.log('ism:' + name)
+console.log('familiya:' + surname);
+console.log('yosh:' + age);
+console.log('Yoqtirgan fan:'+ fan);
+console.log('Yashash manzili:' + adres);
